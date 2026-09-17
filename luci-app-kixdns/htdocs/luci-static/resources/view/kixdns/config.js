cb531aaeba56a5b55b18492cab21ec3d8251c4f1
JohnsonRan <Johnson_Ran@hotmail.com>
2026-09-17 11:46:15 +0900

    chore(release): prepare v1.5.4

--- luci-app-kixdns/htdocs/luci-static/resources/view/kixdns/config.js
+++ luci-app-kixdns/htdocs/luci-static/resources/view/kixdns/config.js
@@ -34,7 +34,7 @@ return view.extend({
 		var operationPending = false;
 		var frame = E('iframe', {
 			'class': 'kixdns-config-editor-frame',
-			'src': L.resource('kixdns/config_editor.html') + '?v=1.5.3',
+			'src': L.resource('kixdns/config_editor.html') + '?v=1.5.4',
 			'title': _('KixDNS configuration editor')
 		});
 
